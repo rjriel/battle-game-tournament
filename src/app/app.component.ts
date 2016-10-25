15161d2fc3399a953eb5c139094ffb58ad91fa99
Rene-Jacques Riel <rie@qlik.com>
2016-10-25 13:09:49 -0400

    added support for the rest of the tournament display added audio and icons for hit/miss/heal actions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import {Http, Headers} from '@angular/http'
 import {DomSanitizer} from '@angular/platform-browser'
 import * as $ from "jquery"
 
-let emojis = ["1f40a","1f40b","1f40c","1f40d","1f40e","1f40f","1f41a","1f41b","1f41c","1f41d","1f41e","1f41f","1f42a","1f42b","1f42c","1f42d","1f42e","1f42f","1f43a","1f43b","1f43c"]
+let emojis = ["1f40a", "1f40b", "1f40c", "1f40d", "1f40e", "1f40f", "1f41a", "1f41b", "1f41c", "1f41d", "1f41e", "1f41f", "1f42a", "1f42b", "1f42c", "1f42d", "1f42e", "1f42f", "1f43a", "1f43b", "1f43c"]
 
 @Component({
   selector: 'app-root',
@@ -13,30 +13,36 @@ let emojis = ["1f40a","1f40b","1f40c","1f40d","1f40e","1f40f","1f41a","1f41b","1
 })
 export class AppComponent {
   title: string = "Qlikémon"
-  round: string = "registering..."
-  players: Object[] = []
+  round: string = "registering"
+  players: { health: number, _id: string, attack: number, wins: number }[] = []
   semiFinalists: Object[] = []
   finalists: Object[] = []
   champion: Object
   socket: any
   headers: Headers
+  currentBracket: number = 0
+  animations: { action: string, value: number, result: string, player: string }[] = []
+  currentGame: { player1: string, player2: string }
+  hitSound = new Audio('/assets/hit.m4a')
+  missSound = new Audio('/assets/miss.m4a')
+  healSound = new Audio('/assets/heal.m4a')
 
   constructor(private http: Http, private sanitizer: DomSanitizer) {
-    for(let i = 1; i < 5; i++) {
+    for (let i = 1; i < 5; i++) {
       this.semiFinalists.push({
         name: `Semi Finalist ${i}`,
-        background: sanitizer.bypassSecurityTrustStyle("white")
+        icon: "/assets/26d4.png"
       })
     }
-    for(let i = 1; i < 3; i++) {
+    for (let i = 1; i < 3; i++) {
       this.finalists.push({
         name: `Finalist ${i}`,
-        background: sanitizer.bypassSecurityTrustStyle("white")
+        icon: "/assets/26d4.png"
       })
     }
     this.champion = {
       name: 'Champion',
-      background: sanitizer.bypassSecurityTrustStyle("white")
+      icon: "/assets/26d4.png"
     }
     let id: string = '4a3147ff-e299-401e-8c80-fe2d8247855e';
     let secret: string = 'b3409604-08a6-4854-892d-41bcead8c823';
@@ -53,7 +59,6 @@ export class AppComponent {
       this.getPlayer(player.id)
         .then((playerData) => {
           this.addPlayer(playerData)
-
         })
     }.bind(this));
     this.socket.on('player disconnected', function (player) {
@@ -64,26 +69,59 @@ export class AppComponent {
         }
       }
     }.bind(this));
-    this.socket.on('new round',(round) => {
-      this.round = round
-    })
-    this.socket.on('move played', function (move) {
-      if (move.action === "attack" && move.result != "miss") {
-        this.players.filter((player) => player._id === move.player)[0].attack += move.value
-      }
-    }.bind(this));
-    this.socket.on('game over', function (game) {
-      switch(this.round) {
+    this.socket.on('new round', (round) => {
+      switch (round) {
         case "round robin":
-          this.players.filter((player) => player._id === game.winner)[0].wins += 1
+          this.round = "rr"
           break;
         case "quarter finals":
-          let quarterFinalWinner = this.players.filter((player) => player._id === game.winner)[0]
-          this.semiFinalists.shift()
+          this.round = "qf"
           break;
         case "semi finals":
+          this.round = "sf"
           break;
         case "finals":
+          this.round = "f"
+          break;
+      }
+      this.players.forEach((player) => {
+        player.health = 100
+      })
+    })
+    this.socket.on('start game', (gameId) => {
+      this.getGame(gameId.id)
+        .then((game: { player1: string, player2: string }) => {
+          this.currentGame = game
+        })
+    })
+    this.socket.on('move played', function (move) {
+      if (move.result !== "heal" && move.result !== "miss" && this.round === "rr") {
+        this.players.filter((player) => player._id === move.player)[0].attack += move.value
+      }
+      if (this.round !== "rr") {
+        this.animations.push(move)
+      }
+    }.bind(this));
+    this.socket.on('game over', function (gameData) {
+      switch (this.round) {
+        case "rr":
+          this.players.filter((player) => player._id === gameData.game.winner)[0].wins += 1
+          if (gameData.winningMove && gameData.winningMove.action === "attack") {
+            this.players.filter((player) => player._id === gameData.winningMove.player)[0].attack += gameData.winningMove.value
+          }
+          break;
+        case "qf":
+          let quarterFinalWinner = this.players.filter((player) => player._id === gameData.game.winner)[0]
+          this.semiFinalists.splice(this.currentBracket, 1, quarterFinalWinner)
+          this.currentBracket += 1
+          break;
+        case "sf":
+          let semiFinalWinner = this.players.filter((player) => player._id === gameData.game.winner)[0]
+          this.finalists.splice(this.currentBracket - 4, 1, semiFinalWinner)
+          this.currentBracket += 1
+          break;
+        case "f":
+          this.champion = this.players.filter((player) => player._id === gameData.game.winner)[0]
           break;
       }
     }.bind(this));
@@ -96,17 +134,55 @@ export class AppComponent {
           this.addPlayer(player)
         })
       })
+
+    setInterval(this.showAnimation.bind(this), 500)
+  }
+
+  showAnimation() {
+    if (this.animations && this.animations.length > 0) {
+      let move = this.animations.shift()
+      let selector = "img#"
+      let sound
+      switch (move.result) {
+        case "heal":
+          selector += "heal-"
+          sound = this.healSound
+          let healedPlayer = move.player === this.currentGame.player1 ? this.currentGame.player1 : this.currentGame.player2
+          this.players.filter((player) => player._id === healedPlayer)[0].health += move.value
+          break;
+        case "miss":
+          selector += "miss-"
+          sound = this.missSound
+          break;
+        default:
+          selector += "hit-"
+          sound = this.hitSound
+          let hitPlayer = move.player === this.currentGame.player1 ? this.currentGame.player2 : this.currentGame.player1
+          this.players.filter((player) => player._id === hitPlayer)[0].health -= move.value
+          break;
+      }
+      selector += `${this.round}-`
+      let isPlayer1 = (move.player === this.currentGame.player2 && move.action === "attack") || (move.player === this.currentGame.player1 && move.action === "heal")
+      selector += isPlayer1 ? this.currentGame.player1 : this.currentGame.player2
+      let animationElement = $(selector)
+      animationElement.show()
+      sound.play()
+      setTimeout(() => {
+        animationElement.hide()
+      }, 250)
+    }
   }
 
   moveThem() {
-    $(".rr-top-8").toggleClass("quarter");
+    this.round = "qf"
   }
 
   addPlayer(player) {
     player.wins = 0
     player.attack = 0
+    player.health = 0
 
-    let icon =  emojis.splice(Math.floor(Math.random() * emojis.length),1)
+    let icon = emojis.splice(Math.floor(Math.random() * emojis.length), 1)
     player.icon = `/assets/${icon}.png`
     this.players.push(player)
   }
@@ -119,6 +195,18 @@ export class AppComponent {
         .subscribe(
           player => resolve(player.json()),
           error => console.log("getPlayer Error", error)
+        )
+    })
+  }
+
+  getGame(gameId) {
+    return new Promise((resolve, reject) => {
+      let url = `http://localhost:8080/games/${gameId}`
+
+      this.http.get(url, {headers: this.headers})
+        .subscribe(
+          game => resolve(game.json()),
+          error => console.log("getGame Error", error)
         )
     })
   }
