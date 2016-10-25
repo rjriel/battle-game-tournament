d5b474f80136eca2e2cba2a3f0ecf2bc2a66d5ec
Rene-Jacques Riel <rie@qlik.com>
2016-10-25 15:19:10 -0400

    adding needed assets separating quarter finalists from players adding winner sound and icon adding attack data to tournament bracket moving game over into animations adding attack/miss/heal animations to rest of tournament bracket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,33 +14,42 @@ let emojis = ["1f40a", "1f40b", "1f40c", "1f40d", "1f40e", "1f40f", "1f41a", "1f
 export class AppComponent {
   title: string = "Qlikémon"
   round: string = "registering"
-  players: { health: number, _id: string, attack: number, wins: number }[] = []
-  semiFinalists: Object[] = []
-  finalists: Object[] = []
-  champion: Object
+  players: { _id: string, attack: number, wins: number, icon: string, name: string }[] = []
+  quarterFinalists: { health: number, _id: string, icon: string, name: string, attack: number }[] = []
+  semiFinalists: { health: number, _id: string, icon: string, name: string, attack: number }[] = []
+  finalists: { health: number, _id: string, icon: string, name: string, attack: number }[] = []
+  champion: { name: string, icon: string, _id: string }
   socket: any
   headers: Headers
   currentBracket: number = 0
-  animations: { action: string, value: number, result: string, player: string }[] = []
+  animations: any[] = []
   currentGame: { player1: string, player2: string }
   hitSound = new Audio('/assets/hit.m4a')
   missSound = new Audio('/assets/miss.m4a')
   healSound = new Audio('/assets/heal.m4a')
+  winnerSound = new Audio('/assets/winner.m4a')
 
   constructor(private http: Http, private sanitizer: DomSanitizer) {
     for (let i = 1; i < 5; i++) {
       this.semiFinalists.push({
+        _id: `semiFinalist${i}`,
         name: `Semi Finalist ${i}`,
-        icon: "/assets/26d4.png"
+        icon: "/assets/26d4.png",
+        health: 0,
+        attack: 0
       })
     }
     for (let i = 1; i < 3; i++) {
       this.finalists.push({
+        _id: `finalist${i}`,
         name: `Finalist ${i}`,
-        icon: "/assets/26d4.png"
+        icon: "/assets/26d4.png",
+        health: 0,
+        attack: 0
       })
     }
     this.champion = {
+      _id: "champion",
       name: 'Champion',
       icon: "/assets/26d4.png"
     }
@@ -75,6 +84,15 @@ export class AppComponent {
           this.round = "rr"
           break;
         case "quarter finals":
+          this.players.slice(0,8).forEach((player) => {
+            this.quarterFinalists.push({
+              _id: player._id,
+              name: player.name,
+              health: 100,
+              attack: 0,
+              icon: player.icon
+            })
+          })
           this.round = "qf"
           break;
         case "semi finals":
@@ -84,16 +102,13 @@ export class AppComponent {
           this.round = "f"
           break;
       }
-      this.players.forEach((player) => {
-        player.health = 100
-      })
     })
-    this.socket.on('start game', (gameId) => {
+    this.socket.on('start game', function(gameId) {
       this.getGame(gameId.id)
         .then((game: { player1: string, player2: string }) => {
           this.currentGame = game
         })
-    })
+    }.bind(this))
     this.socket.on('move played', function (move) {
       if (move.result !== "heal" && move.result !== "miss" && this.round === "rr") {
         this.players.filter((player) => player._id === move.player)[0].attack += move.value
@@ -103,26 +118,14 @@ export class AppComponent {
       }
     }.bind(this));
     this.socket.on('game over', function (gameData) {
-      switch (this.round) {
-        case "rr":
-          this.players.filter((player) => player._id === gameData.game.winner)[0].wins += 1
-          if (gameData.winningMove && gameData.winningMove.action === "attack") {
-            this.players.filter((player) => player._id === gameData.winningMove.player)[0].attack += gameData.winningMove.value
-          }
-          break;
-        case "qf":
-          let quarterFinalWinner = this.players.filter((player) => player._id === gameData.game.winner)[0]
-          this.semiFinalists.splice(this.currentBracket, 1, quarterFinalWinner)
-          this.currentBracket += 1
-          break;
-        case "sf":
-          let semiFinalWinner = this.players.filter((player) => player._id === gameData.game.winner)[0]
-          this.finalists.splice(this.currentBracket - 4, 1, semiFinalWinner)
-          this.currentBracket += 1
-          break;
-        case "f":
-          this.champion = this.players.filter((player) => player._id === gameData.game.winner)[0]
-          break;
+      if(this.round === "rr") {
+        this.players.filter((player) => player._id === gameData.game.winner)[0].wins += 1
+        if (gameData.winningMove && gameData.winningMove.action === "attack") {
+          this.players.filter((player) => player._id === gameData.winningMove.player)[0].attack += gameData.winningMove.value
+        }
+      } else {
+        this.animations.push(gameData.winningMove)
+        this.animations.push(gameData.game)
       }
     }.bind(this));
     this.socket.on('invalid', function (error) {
@@ -138,17 +141,70 @@ export class AppComponent {
     setInterval(this.showAnimation.bind(this), 500)
   }
 
+  gameOver(game) {
+    switch (this.round) {
+      case "qf":
+        let quarterFinalWinner: { _id: string, icon: string, name: string, health: number, attack: number } = this.quarterFinalists.filter((player) => player._id === game.winner)[0]
+        $(`#winner-${this.round}-${quarterFinalWinner._id}`).show()
+        quarterFinalWinner = {
+          _id: quarterFinalWinner._id,
+          icon: quarterFinalWinner.icon,
+          name: quarterFinalWinner.name,
+          health: 100,
+          attack: 0
+        }
+        this.semiFinalists.splice(this.currentBracket, 1, quarterFinalWinner)
+        this.currentBracket += 1
+        break;
+      case "sf":
+        let semiFinalWinner: { _id: string, icon: string, name: string, health: number, attack: number } = this.semiFinalists.filter((player) => player._id === game.winner)[0]
+        $(`#winner-${this.round}-${semiFinalWinner._id}`).show()
+        semiFinalWinner = {
+          _id: semiFinalWinner._id,
+          icon: semiFinalWinner.icon,
+          name: semiFinalWinner.name,
+          health: 100,
+          attack: 0
+        }
+        this.finalists.splice(this.currentBracket - 4, 1, semiFinalWinner)
+        this.currentBracket += 1
+        break;
+      case "f":
+        this.champion = this.players.filter((player) => player._id === game.winner)[0]
+        $(`#winner-${this.round}-${this.champion._id}`).show()
+        break;
+    }
+    this.winnerSound.play()
+  }
+
   showAnimation() {
     if (this.animations && this.animations.length > 0) {
       let move = this.animations.shift()
+      if (move.winner) {
+        // move is not in fact a move, but a game, indicating a game over
+        this.gameOver(move)
+        return
+      }
       let selector = "img#"
       let sound
+      let group
+      switch (this.round) {
+        case "qf":
+          group = this.quarterFinalists
+          break;
+        case "sf":
+          group = this.semiFinalists
+          break;
+        case "f":
+          group = this.finalists
+          break;
+      }
       switch (move.result) {
         case "heal":
           selector += "heal-"
           sound = this.healSound
           let healedPlayer = move.player === this.currentGame.player1 ? this.currentGame.player1 : this.currentGame.player2
-          this.players.filter((player) => player._id === healedPlayer)[0].health += move.value
+          group.filter((player) => player._id === healedPlayer)[0].health += move.value
           break;
         case "miss":
           selector += "miss-"
@@ -158,7 +214,8 @@ export class AppComponent {
           selector += "hit-"
           sound = this.hitSound
           let hitPlayer = move.player === this.currentGame.player1 ? this.currentGame.player2 : this.currentGame.player1
-          this.players.filter((player) => player._id === hitPlayer)[0].health -= move.value
+          group.filter((player) => player._id === move.player)[0].attack += move.value
+          group.filter((player) => player._id === hitPlayer)[0].health -= move.value
           break;
       }
       selector += `${this.round}-`
@@ -173,14 +230,9 @@ export class AppComponent {
     }
   }
 
-  moveThem() {
-    this.round = "qf"
-  }
-
   addPlayer(player) {
     player.wins = 0
     player.attack = 0
-    player.health = 0
 
     let icon = emojis.splice(Math.floor(Math.random() * emojis.length), 1)
     player.icon = `/assets/${icon}.png`
